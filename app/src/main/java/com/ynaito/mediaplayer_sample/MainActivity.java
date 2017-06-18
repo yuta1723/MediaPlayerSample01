e01c37360181fed2325f9daec19b27870eb58a50
yuta naito <y.naito@suvt.co.jp>
2017-06-19 01:20:53 +0900

    play / pauseの機能を追加し、動画のサイズに合わせてviewのサイズを変更する機能を追加

--- app/src/main/java/com/ynaito/mediaplayer_sample/MainActivity.java
+++ app/src/main/java/com/ynaito/mediaplayer_sample/MainActivity.java
@@ -1,46 +1,131 @@
 package com.ynaito.mediaplayer_sample;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
     private String TAG = MainActivity.class.getSimpleName();
+
+    private Context mContext;
+    private String uriString = "http://domain/path/content.mp4";
+    MediaPlayer mMediaPlayer = null;
+    SurfaceView mSurfaceView = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
-        Log.d(TAG,"onCreate");
-    }
+        mContext = this;
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+        mSurfaceView.getHolder().addCallback(this);
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG,"onRestart");
+        mSurfaceView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.pause();
+                } else if ((mMediaPlayer != null && !mMediaPlayer.isPlaying())) {
+                    mMediaPlayer.start();
+                }
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG,"onResume");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG,"onDestroy");
+        Log.d(TAG, "onResume");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG,"onStart");
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG,"onStop");
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+        }
+        mMediaPlayer = null;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        Log.d(TAG, "surfaceCreated");
+        mMediaPlayer = new MediaPlayer();
+        try {
+            mMediaPlayer.setDataSource(this, Uri.parse(uriString));
+            mMediaPlayer.setDisplay(surfaceHolder);
+            mMediaPlayer.prepare();
+        } catch (IOException e) {
+            Log.d(TAG, "Stream read error : " + e);
+            return;
+        }
+        setVideoLayoutParams();
+        mMediaPlayer.start();
+    }
+
+    private void setVideoLayoutParams() {
+        if (mMediaPlayer == null || mSurfaceView == null) {
+            return;
+        }
+        Display display = this.getWindowManager().getDefaultDisplay();
+        int videoWidth = mMediaPlayer.getVideoWidth();
+        int videoHeight = mMediaPlayer.getVideoHeight();
+        int surfaceHeight = (int) (display.getWidth() * (1.0 * videoHeight / videoWidth));
+        Log.d(TAG,"display.getHeight" + surfaceHeight);
+        mSurfaceView.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, surfaceHeight));
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        Log.d(TAG, "surfaceChanged");
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        Log.d(TAG, "surfaceDestroyed");
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+        }
+        mMediaPlayer = null;
     }
 }
+
